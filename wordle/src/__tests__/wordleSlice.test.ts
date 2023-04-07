39c79f5b5b301fe316579beca2195cf7d9c024d7
Kuba Pawlak <kubapawlak1051@wp.pl>
2023-04-07 23:18:26 +0200

    Prepared for writing tests of wordleSlice.ts

---
+++ wordle/src/__tests__/wordleSlice.test.ts
@@ -0,0 +1,15 @@
+describe("wordleSlice", () => {
+  it("should reset the game state correctly", () => {});
+
+  it("should add a letter to the current row correctly", () => {});
+
+  it("should not add a letter when the game is not playing", () => {});
+
+  it("should not add a letter when the current row is already full", () => {});
+
+  it("should remove the last letter from the current row correctly", () => {});
+
+  it("should not remove a letter when the game is not playing", () => {});
+
+  it("should not remove a letter when the current row is empty", () => {});
+});
