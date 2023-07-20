2892001874704c9090f7728ed78b2ceeac379750
Kuba Pawlak <kubapawlak1051@wp.pl>
2023-07-20 22:32:16 +0200

    Fixed Redux error - using Provider in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+'use client';
+
+import { Provider } from 'react-redux';
+
 import Game from '@/components/Game';
+import Header from '@/components/UI/Header';
+import Message from '@/components/UI/Message';
+import { store } from '@/redux/store';
 
 export default function Home() {
-  return <Game />;
+  return (
+    <Provider store={store}>
+      <Message />
+      <Header />
+      <Game />
+    </Provider>
+  );
 }
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,10 +1,4 @@
-import { Provider } from 'react-redux';
-
 import { useDispatchWordle } from '@/redux/slices/wordleSlice';
-import { store } from '@/redux/store';
-
-import Header from '../UI/Header';
-import Message from '../UI/Message';
 
 import Board from './Gameboard';
 import Keyboard from './Keyboard';
@@ -15,17 +9,13 @@ export default function Game() {
   const { addLetter, removeLetter, submitGuess } = useDispatchWordle();
 
   return (
-    <Provider store={store}>
-      <Header />
-      <Message />
-      <main className={styles.game}>
-        <Board />
-        <Keyboard
-          onBackspaceClick={() => removeLetter()}
-          onEnterClick={() => submitGuess()}
-          onLetterClick={(letter: string) => addLetter(letter)}
-        />
-      </main>
-    </Provider>
+    <main className={styles.game}>
+      <Board />
+      <Keyboard
+        onBackspaceClick={() => removeLetter()}
+        onEnterClick={() => submitGuess()}
+        onLetterClick={(letter: string) => addLetter(letter)}
+      />
+    </main>
   );
 }
