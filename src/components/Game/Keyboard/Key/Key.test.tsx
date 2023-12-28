b7ffcdcd0ae4aff59c1cdce73184f01b0f2a1898
Kuba Pawlak <kubapawlak1051@wp.pl>
2023-12-28 21:26:22 +0100

    test: tests for Key, Keyboard and Row components

---
+++ src/components/Game/Keyboard/Key/Key.test.tsx
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { Key } from '.';
+
+describe('Key', () => {
+  it('renders with default props', () => {
+    const { asFragment } = render(<Key onClick={() => {}}>A</Key>);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('displays provided letter', () => {
+    render(<Key onClick={() => {}}>A</Key>);
+
+    screen.getByText('A');
+    expect(screen.getByTestId('key')).toHaveTextContent('A');
+  });
+
+  it('injects optional props when they are specified', () => {
+    render(
+      <Key onClick={() => {}} ariaLabel='A' dataKey='A' letterState='present'>
+        A
+      </Key>
+    );
+
+    expect(screen.getByTestId('key')).toHaveAttribute('aria-label', 'A');
+    expect(screen.getByTestId('key')).toHaveAttribute('data-key', 'A');
+    expect(screen.getByTestId('key')).toHaveAttribute('data-state', 'present');
+  });
+
+  it('assigns proper class name when large prop is true', () => {
+    render(
+      <Key onClick={() => {}} large>
+        A
+      </Key>
+    );
+    expect(screen.getByTestId('key')).toHaveClass('key--large');
+  });
+
+  it('invokes onClick function when clicked', () => {
+    const onClick = jest.fn();
+    render(<Key onClick={onClick}>A</Key>);
+
+    fireEvent.click(screen.getByTestId('key'));
+    expect(onClick).toHaveBeenCalled();
+  });
+});
---
+++ src/components/Game/Keyboard/Keyboard.test.tsx
@@ -0,0 +1,93 @@
+import { fireEvent, screen } from '@testing-library/react';
+
+import { initialState } from '@/redux/slices/wordleSlice';
+import { type LetterState } from '@/types';
+import { renderWithProviders } from '@/utils/test-utils';
+
+import { Keyboard } from '.';
+
+describe('Keyboard', () => {
+  it('renders all keys', () => {
+    renderWithProviders(
+      <Keyboard
+        onBackspaceClick={() => {}}
+        onEnterClick={() => {}}
+        onLetterClick={(_letter) => {}}
+      />
+    );
+
+    const keys = screen.getAllByTestId('key');
+    expect(keys).toHaveLength(28); // 26 letters + enter + backspace
+  });
+
+  it('assigns to keys proper letterState', () => {
+    const keyboardLetterState: Record<string, LetterState> = {
+      a: 'correct',
+      b: 'present',
+      c: 'absent',
+    };
+
+    renderWithProviders(
+      <Keyboard
+        onBackspaceClick={() => {}}
+        onEnterClick={() => {}}
+        onLetterClick={(_letter) => {}}
+      />,
+      { preloadedState: { wordle: { ...initialState, keyboardLetterState } } }
+    );
+
+    const keys = screen.getAllByTestId('key');
+    const keysWithLetterState = keys.filter((key) => keyboardLetterState[key.textContent!]);
+
+    keysWithLetterState.forEach((key) => {
+      const letter = key.textContent!;
+      const expectedState = keyboardLetterState[letter];
+      expect(key).toHaveAttribute('data-state', expectedState);
+    });
+  });
+
+  it('invokes all onClick handlers', () => {
+    const onBackspaceClick = jest.fn();
+    const onEnterClick = jest.fn();
+    const onLetterClick = jest.fn();
+
+    renderWithProviders(
+      <Keyboard
+        onBackspaceClick={onBackspaceClick}
+        onEnterClick={onEnterClick}
+        onLetterClick={onLetterClick}
+      />
+    );
+
+    fireEvent.keyUp(window, { key: 'Backspace' });
+    fireEvent.keyUp(window, { key: 'Enter' });
+    fireEvent.keyUp(window, { key: 'a' });
+
+    expect(onBackspaceClick).toHaveBeenCalledTimes(1);
+    expect(onEnterClick).toHaveBeenCalledTimes(1);
+    expect(onLetterClick).toHaveBeenCalledWith('a');
+  });
+
+  it('does not invoke any onClick handlers when game status is not "playing"', () => {
+    const onBackspaceClick = jest.fn();
+    const onEnterClick = jest.fn();
+    const onLetterClick = jest.fn();
+
+    renderWithProviders(
+      <Keyboard
+        onBackspaceClick={onBackspaceClick}
+        onEnterClick={onEnterClick}
+        onLetterClick={onLetterClick}
+      />,
+      { preloadedState: { wordle: { ...initialState, status: 'win' } } }
+    );
+
+    fireEvent.keyUp(window, { key: 'Backspace' });
+    fireEvent.keyUp(window, { key: 'Enter' });
+    fireEvent.keyUp(window, { key: 'a' });
+
+    expect(onBackspaceClick).not.toHaveBeenCalled();
+    expect(onEnterClick).not.toHaveBeenCalled();
+    expect(onLetterClick).not.toHaveBeenCalled();
+  });
+});
